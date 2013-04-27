b5aa10012662c6f2236db2c8826a5b4a3cefd7e1
Paulo Perez Prieto <ing.pauloperez@gmail.com>
2013-04-27 18:17:24 -0300

    Alta de Proyectos: Dominio

--- laurel/src/main/java/edu/laurel/Laurel.java
+++ laurel/src/main/java/edu/laurel/Laurel.java
@@ -2,31 +2,43 @@ package edu.laurel;
 
 
 public class Laurel {
-	private java.util.Set<Usuario> usuarios;
-	private java.util.Collection<Proyecto> proyectos;
-	
+	private final java.util.Set<Usuario> usuarios;
+	private final java.util.Collection<Proyecto> proyectos;
+
 	public Laurel() {
 		usuarios = new java.util.HashSet<Usuario>();
 		proyectos = new java.util.ArrayList<Proyecto>(5);
 	}
-	
-	public void registrar(Proyecto proyecto){
+
+	public java.util.Collection<Proyecto> getProyectos() {
+		return proyectos;
+	}
+
+	public boolean estaRegistrado(final Proyecto proyecto) {
+		return proyectos.contains(proyecto);
+	}
+
+	public void registrar(final Proyecto proyecto){
 		proyectos.add(proyecto);
 	}
-	
-	public java.util.Set<Usuario> lista() {
+
+	public void borrar(final Proyecto proyecto) {
+		proyectos.remove(proyecto);
+	}
+
+	public java.util.Set<Usuario> getUsuarios() {
 		return usuarios;
 	}
-	
-	public boolean estaRegistrado(Usuario usuario) {
+
+	public boolean estaInscripto(final Usuario usuario) {
 		return usuarios.contains(usuario);
 	}
 
-	public void darAlta(Usuario  usuario) {
+	public void darAlta(final Usuario  usuario) {
 		usuarios.add(usuario);
 	}
 
-	public void darBaja(Usuario usuario) {
+	public void darBaja(final Usuario usuario) {
 		usuarios.remove(usuario);
 	}
 }
