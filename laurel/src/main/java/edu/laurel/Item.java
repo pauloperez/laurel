875282f2ce362e6f5df3104c4e81a8603ceb6e50
Barba Red <Barba Red@Calacho>
2013-05-05 18:41:26 -0300

    Item y Proyecto
    Signed-off-by: Barba Red <Barba Red@Calacho>

--- laurel/src/main/java/edu/laurel/Item.java
+++ laurel/src/main/java/edu/laurel/Item.java
@@ -1,10 +1,61 @@
 package edu.laurel;
 
+import java.util.Date;
+
 public class Item {
 	private Prioridad prioridad;
 	private TipoItem tipo;
-	private java.util.Deque<EstadoItem> camino;
-
+	private final java.util.Deque<EstadoItem> camino;
 	private Usuario responsable;
+	private final Date creacion;
+	private String descripcion;
+
+	public Item(final TipoItem tipo, final Prioridad prioridad) {
+		this.prioridad = prioridad;
+		this.tipo = tipo;
+		this.creacion = new Date();
+		camino = new java.util.ArrayDeque<EstadoItem>();
+
+	}
+
+	public Prioridad getPrioridad() {
+		return prioridad;
+	}
+
+	public void setPrioridad(final Prioridad prioridad) {
+		this.prioridad = prioridad;
+	}
+
+	public TipoItem getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(final TipoItem tipo) {
+		this.tipo = tipo;
+	}
+
+	public Usuario getResponsable() {
+		return responsable;
+	}
+
+	public void setResponsable(final Usuario responsable) {
+		this.responsable = responsable;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(final String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public java.util.Deque<EstadoItem> getCamino() {
+		return camino;
+	}
+
+	public Date getCreacion() {
+		return creacion;
+	}
 
 }
--- laurel/src/main/java/edu/laurel/Proyecto.java
+++ laurel/src/main/java/edu/laurel/Proyecto.java
@@ -2,6 +2,7 @@ package edu.laurel;
 
 public class Proyecto {
 	private final String nombre;
+	private final java.util.Collection<Item> items;
 	private final java.util.Collection<TipoItem> tipoItems;
 
 	private final Usuario lider;
@@ -10,6 +11,7 @@ public class Proyecto {
 	public Proyecto(final String nombre, final Usuario lider) {
 		this.nombre = nombre;
 		this.lider = lider;
+		items = new java.util.ArrayList<Item>();
 		miembros = new java.util.ArrayList<Usuario>();
 		tipoItems =  new java.util.HashSet<TipoItem>();
 	}
@@ -31,6 +33,10 @@ public class Proyecto {
 
 	public java.util.Collection<TipoItem> getTipoItems() {
 		return tipoItems;
+	}
+
+	public java.util.Collection<Item> getItems() {
+		return items;
 	}
 
 	public java.util.Collection<Usuario> getMiembros() {
