2d6066adb3c3a26189e9d0f3bc729884c7a8406c
Paulo Perez Prieto <ing.pauloperez@gmail.com>
2013-10-20 22:30:13 -0300

    Ejemplo interaccion capa presentacion
    Signed-off-by: Paulo Perez Prieto <ing.pauloperez@gmail.com>

--- laurel/src/main/java/edu/laurel/aplicacion/ServiciosUsuario.java
+++ laurel/src/main/java/edu/laurel/aplicacion/ServiciosUsuario.java
@@ -4,10 +4,16 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import edu.laurel.dominio.Usuario;
+
 @Stateless
 public class ServiciosUsuario {
 
     @PersistenceContext(unitName = "laurel-pu")
     private EntityManager entityManager;
+
+    public void alta(final Usuario usuario) {
+	entityManager.persist(usuario);
+    }
 
 }
--- laurel/src/main/java/edu/laurel/presentacion/InterfazUsuarios.java
+++ laurel/src/main/java/edu/laurel/presentacion/InterfazUsuarios.java
@@ -1,18 +1,38 @@
 package edu.laurel.presentacion;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import edu.laurel.aplicacion.ServiciosUsuario;
+import edu.laurel.dominio.Usuario;
 
 @Named
+@ViewScoped
 public class InterfazUsuarios {
 
     @Inject
     ServiciosUsuario serviciosUsuario;
 
-    public void altaUsuario() {
+    private String nombreUsuario;
 
+    @PostConstruct
+    public void inicio() {
+	nombreUsuario = "Un nombre culaquiera";
+    }
+
+    public void alta() {
+	final Usuario usuario = new Usuario(nombreUsuario);
+	serviciosUsuario.alta(usuario);
+    }
+
+    public String getNombreUsuario() {
+	return nombreUsuario;
+    }
+
+    public void setNombreUsuario(final String nombreUsuario) {
+	this.nombreUsuario = nombreUsuario;
     }
 
 }
